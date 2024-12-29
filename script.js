5b1118bb897b3e9d8e566bfb3cc687669e611664
kartik132000 <sujeetkumarsen37@gmail.com>
2024-12-29 16:54:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,7 +9,25 @@ function convertToRoman(num) {
       6:['I', 1]
     };
 
-  //your code here
+  //your code herelet result = '';
+
+    // Iterate through the object to build the Roman numeral
+    for (let i = 0; i < Object.keys(obj).length; i++) {
+        const [roman, value] = obj[i];
+
+        // Add the Roman numeral to the result as many times as the value fits into num
+        while (num >= value) {
+            result += roman;
+            num -= value;
+        }
+    }
+
+    return result;
+}
+
+// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 14, uncomment the following line
+
+console.log(convertToRoman(14)); // Should print XIV
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
